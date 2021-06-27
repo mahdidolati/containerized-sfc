6a5545e0a0e73e7c2662740ea66f41cbb9770cf2
myrunmachine <86378214+myrunmachine@users.noreply.github.com>
2021-06-27 10:26:09 -0700

    minor early results

--- main.py
+++ main.py
@@ -54,7 +54,8 @@ def share_percentage_test(inter_arrival):
     #
     iterations = 5
     arrival_rate = 1.0 / inter_arrival
-    n_share_ps = np.arange(0.05, 1.05, 0.05)
+    n_share_ps = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5,
+                  0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0]
     share_percentages = []
     Const.VNF_LAYER = [3, 6]
     Const.LAYER_SIZE = [60, 101]
