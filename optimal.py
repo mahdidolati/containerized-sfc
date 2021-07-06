f99c64d99c436556108cecbf13edde4130e50ae0
myrunmachine <86378214+myrunmachine@users.noreply.github.com>
2021-07-06 14:06:28 -0700

    ilp run on machine

--- optimal.py
+++ optimal.py
@@ -1,6 +1,5 @@
 import cvxpy as cp
 import numpy as np
-from mip_cvxpy import PYTHON_MIP
 
 
 def get_last_t(reqs):
@@ -16,8 +15,8 @@ def solve_optimal(my_net, R, reqs):
     B = my_net.get_all_base_stations()
     E = my_net.get_all_edge_nodes()
     a_var = cp.Variable((len(reqs), ), boolean=True)
-    z_var = cp.Variable((len(E)*len(R)*T, ), boolean=True)
-    y_var = cp.Variable((len(E)*len(R)*T, ), boolean=True)
+    z_var = cp.Variable((len(E)*len(R), T), boolean=True)
+    y_var = cp.Variable((len(E)*len(R), T), boolean=True)
     constraints = []
     # constraints += [z_var + y_var <= np.ones((len(E)*len(R), T))]
     constraints += [cp.sum(a_var[0:3]) <= 2]
@@ -26,8 +25,8 @@ def solve_optimal(my_net, R, reqs):
     objective = cp.Maximize(cp.sum(a_var))
 
     prob = cp.Problem(objective, constraints)
-    prob.solve(solver=cp.CBC, verbose=1)
+    prob.solve(solver=cp.CBC)
 
-    print(a_var[0].value)
+    print("Problem status {} with value {}".format(prob.status, prob.value))
 
     return 0, 0
