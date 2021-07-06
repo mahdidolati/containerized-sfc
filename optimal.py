4dc7594ca138926c0aed2a6255baa1af1f7adf07
mehdi <mehdidolati@hotmail.com>
2021-07-07 01:28:43 +0430

    milp

--- optimal.py
+++ optimal.py
@@ -1,48 +1,33 @@
 import cvxpy as cp
 import numpy as np
+from mip_cvxpy import PYTHON_MIP
 
 
 def get_last_t(reqs):
     t_max = 0
     for r in reqs:
         t_max = max(r.tau2, t_max)
-    return t_max
+    return int(t_max)
 
 
 def solve_optimal(my_net, R, reqs):
-    U = len(reqs)
-    layer_no = len(R)
     T = get_last_t(reqs)
-    wired_link, mm_links = my_net.get_link_sets()
-    all_bases = my_net.get_all_base_stations()
-    all_enodes = my_net.get_all_edge_nodes()
-    a_var = dict()
-    for u in range(U):
-        a_var[u] = cp.Variable(integer=True)
-    z_var = dict()
-    for e in range(len(all_enodes)):
-        for r in range(layer_no):
-            for t in range(T):
-                z_var[(e,r,t)] = cp.Variable(integer=True)
-    y_var = dict()
-    for e in range(len(all_enodes)):
-        for r in range(layer_no):
-            for t in range(T):
-                y_var[(e, r, t)] = cp.Variable(integer=True)
+    Lw, Lm = my_net.get_link_sets()
+    B = my_net.get_all_base_stations()
+    E = my_net.get_all_edge_nodes()
+    a_var = cp.Variable((len(reqs), ), boolean=True)
+    z_var = cp.Variable((len(E)*len(R)*T, ), boolean=True)
+    y_var = cp.Variable((len(E)*len(R)*T, ), boolean=True)
     constraints = []
-    for e in range(len(all_enodes)):
-        for r in range(layer_no):
-            for t in range(T):
-                constraints += [z_var[(e,r,t)] + y_var[(e,r,t)] <= 1]
+    # constraints += [z_var + y_var <= np.ones((len(E)*len(R), T))]
+    constraints += [cp.sum(a_var[0:3]) <= 2]
+    constraints += [cp.sum(a_var[3:16]) <= 1]
 
-    obj = 0
-    for u in a_var:
-        obj += a_var[u]
-    objective = cp.Maximize(obj)
+    objective = cp.Maximize(cp.sum(a_var))
 
     prob = cp.Problem(objective, constraints)
-    prob.solve()
-    for u in a_var:
-        print(a_var[u].value)
+    prob.solve(solver=cp.CBC, verbose=1)
+
+    print(a_var[0].value)
 
     return 0, 0
