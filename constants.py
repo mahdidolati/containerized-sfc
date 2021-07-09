da87154435124e4c96a024d734f066b8e282d62b
myrunmachine <86378214+myrunmachine@users.noreply.github.com>
2021-07-09 08:26:53 -0700

    big lambda test

--- constants.py
+++ constants.py
@@ -1,16 +1,16 @@
 class Const:
-    VNF_NUM = 10
-    VNF_CPU = [0.1, 0.4]
-    VNF_RAM = [0.1, 0.3]
+    VNF_NUM = 15
+    VNF_CPU = [0.1, 0.15]
+    VNF_RAM = [0.1, 0.15]
     VNF_LAYER = [5, 12]  # [5, 12]
     TAU1 = [3, 8]
     TAU2 = [10, 20]
-    LAYER_NUM = 15
-    LAYER_SIZE = [15, 350]  # [2, 70]
+    LAYER_NUM = 20
+    LAYER_SIZE = [50, 350]  # [2, 70]
     SFC_LEN = [1, 7]
     SFC_DELAY = [50, 500]
     ALPHA_RANGE = [0.8, 1.05]
-    LAMBDA_RANGE = [1, 5]
+    LAMBDA_RANGE = [10, 50]
     #
     WIRE_LINK_PR = 0.2
     MM_MAX_DIST = 2.0
