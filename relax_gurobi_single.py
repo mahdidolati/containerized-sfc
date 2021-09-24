eed8d9297e66d5fa346e85aeb8c5f1e20f160d91
mehdi <mehdidolati@hotmail.com>
2021-09-24 12:56:08 +0330

    round routing vars, q

--- relax_gurobi_single.py
+++ relax_gurobi_single.py
@@ -46,6 +46,12 @@ def solve_single_relax(my_net, R, Rvol, req):
         N_id[b] = n_idx
         n_idx = n_idx + 1
 
+    L_id = dict()
+    l_idx = 0
+    for l in range(len(L)):
+        L_id[L[l]] = l_idx
+        l_idx = l_idx + 1
+
     pre_computed_paths = dict()
     for e in E:
         pre_computed_paths[N_id[e]] = my_net.pre_compute_paths(e, req.arrival_time)
@@ -268,42 +274,51 @@ def solve_single_relax(my_net, R, Rvol, req):
 
 
     tol_val = 1e-4
-    rounded = set()
-    for _ in range(len(req.vnfs)):
+    loc_of = dict()
+    for i in range(len(req.vnfs)):
         v_max = 0
-        v_max_id = None
-        for i in range(len(req.vnfs)):
-            if i in rounded:
-                continue
-            for n in range(N):
-                a = m.getVarByName("v[{},{}]".format(n, i)).x
-                if v_max_id is None or v_max < a:
-                    v_max_id = (n, i)
-                    v_max = a
+        best_loc = None
+        for n in range(N):
+            a = m.getVarByName("v[{},{}]".format(n, i)).x
+            if best_loc is None or v_max < a:
+                best_loc = n
+                v_max = a
 
-        rounded.add(v_max_id[1])
-        print("rounded {}".format(rounded))
-        v_var[v_max_id[0], v_max_id[1]].lb = 1.0
-
-        if v_max_id[0] < len(E):
-            selected_e = v_max_id[0]
-            selected_i = v_max_id[1]
-            for rr in missing_layers[(selected_e, selected_i)]:
-                r = R_id[rr]
-                best_p = 0
-                best_p_id = None
-                for p in range(len(pre_computed_paths[selected_e])):
-                    a = m.getVarByName("y[{},{},{}]".format(selected_e, p, r)).x
+        v_var[best_loc, i].lb = 1.0
+        loc_of[i] = E[best_loc] if best_loc < len(E) else cloud_node
+        if best_loc < len(E):
+            for rr in missing_layers[(best_loc, i)]:
+                y_max = 0
+                best_path = None
+                for p in range(len(pre_computed_paths[best_loc])):
+                    a = m.getVarByName("y[{},{},{}]".format(best_loc, p, R_id[rr])).x
                     if a > tol_val:
-                        if best_p_id is None or best_p < a:
-                            best_p_id = p
-                            best_p = a
-                if best_p_id is not None:
-                    m.getVarByName("y[{},{},{}]".format(selected_e, best_p_id, r)).lb = 1.0
+                        if best_path is None or y_max < a:
+                            best_path = p
+                            y_max = a
+                if best_path is not None:
+                    m.getVarByName("y[{},{},{}]".format(best_loc, best_path, R_id[rr])).lb = 1.0
+
+        links = []
+        if i == 0:
+            _, _, path_nodes, links = my_net.get_biggest_path(req.entry_point, loc_of[i], req.tau1)
+        elif loc_of[i-1] != loc_of[i]:
+            _, _, path_nodes, links = my_net.get_biggest_path(loc_of[i-1], loc_of[i], req.tau1)
+
+        for l in links:
+            m.getVarByName("q[{},{}]".format(L_id[l], i)).lb = 1.0
 
         m.optimize()
         if m.status == GRB.INFEASIBLE:
             return False, None
+
+    _, _, path_nodes, links = my_net.get_biggest_path(loc_of[len(req.vnfs)-1], req.entry_point, req.tau1)
+    for l in links:
+        m.getVarByName("q[{},{}]".format(L_id[l], len(req.vnfs) + 1)).lb = 1.0
+
+    m.optimize()
+    if m.status == GRB.INFEASIBLE:
+        return False, None
 
     for i in range(len(req.vnfs)):
         for n in range(N):
