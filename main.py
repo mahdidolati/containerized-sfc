0fe5aa264388a4e506057c0e83f778e0bb6159d2
myrunmachine <serverut5188@outlook.com>
2022-01-26 10:44:47 -0800

    kkkkk

--- main.py
+++ main.py
@@ -239,7 +239,7 @@ def optimal_test(inter_arrival, scale_bw=False):
 def scaling_test(inter_arrival):
     np.random.seed(1)
     my_net = NetGenerator().get_g()
-    req_num = 50
+    req_nums = [50]
     sfc_gen = SfcGenerator(my_net, {1: 1.0}, 1.0)
     sfc_gen.print()
     # R_ids = [i for i in sfc_gen.layers]
@@ -257,6 +257,12 @@ def scaling_test(inter_arrival):
              RUNTIME: Stat.MEAN_MODE,
              REVENUE: Stat.MEAN_MODE}
     algs = list()
+    solvers = []
+    for sf in scaling_factors:
+        solver = GurobiSingleRelax(0, sf, "popularity_learn")
+        rgx = "{:.1f}".format(solver.bw_scaler)
+        algs.append(solver.get_name() + rgx)
+        solvers.append(solver)
     stat_collector = StatCollector(algs, stats)
     GROUP = "GROUP"
     stats2 = {GROUP: Stat.SUM_MODE}
@@ -266,61 +272,59 @@ def scaling_test(inter_arrival):
     #
     iterations = 3
     arrival_rate = 1.0 / inter_arrival
-    for sf in scaling_factors:
-        np.random.seed(1)
-        solver = GurobiSingleRelax(0, sf, "popularity_learn")
-        if solver.get_name() not in algs:
-            algs.append(solver.get_name())
-        run_name = "{:.1f}".format(sf)
+    for req_num in req_nums:
+        run_name = "{:d}".format(req_num)
         print("run-name:", run_name)
         for itr in range(iterations):
-            np.random.seed(itr * 4321)
+            np.random.seed(itr * 1234)
             reqs = []
             t = 0
             for _ in range(req_num):
                 reqs.append(sfc_gen.get_chain(t))
                 t = t + int(np.ceil(np.random.exponential(1.0 / arrival_rate)))
                 print(reqs[-1])
-            #
-            if solver.convert_layer:
-                R_ids, R_vols = solver.do_convert_no_share(reqs)
-            else:
-                R_ids = [i for i in sfc_gen.layers]
-                R_vols = sfc_gen.layers
-            solver.set_env(my_net, R_ids, R_vols)
-            t1 = process_time()
-            if solver.batch:
-                tr = solver.solve_batch(my_net, sfc_gen.vnfs_list, reqs)
-            else:
-                tr = test(solver, reqs)
-                print("Solver: {} got {}".format(solver.get_name(), tr))
-            t2 = process_time()
-            stat_collector.add_stat(solver.get_name(), ACCEPT_RATIO, run_name, tr.avg_admit)
-            stat_collector.add_stat(solver.get_name(), DOWNLOAD_LAYER, run_name, tr.avg_dl)
-            stat_collector.add_stat(solver.get_name(), RUNTIME, run_name, t2 - t1)
-            stat_collector.add_stat(solver.get_name(), CHAIN_BW, run_name, tr.chain_bw)
-            stat_collector.add_stat(solver.get_name(), REVENUE, run_name, tr.revenue)
-            for rg in tr.res_groups:
+            for solver in solvers:
+                np.random.seed(itr * 4321)
+                if solver.convert_layer:
+                    R_ids, R_vols = solver.do_convert_no_share(reqs)
+                else:
+                    R_ids = [i for i in sfc_gen.layers]
+                    R_vols = sfc_gen.layers
+                solver.set_env(my_net, R_ids, R_vols)
+                t1 = process_time()
+                if solver.batch:
+                    tr = solver.solve_batch(my_net, sfc_gen.vnfs_list, reqs)
+                else:
+                    tr = test(solver, reqs)
+                    print("Solver: {} got {}".format(solver.get_name(), tr))
+                t2 = process_time()
                 rgx = "{:.1f}".format(solver.bw_scaler)
-                stat_collector2.add_stat(rg, GROUP, rgx, tr.res_groups[rg])
+                stat_collector.add_stat(solver.get_name()+rgx, ACCEPT_RATIO, run_name, tr.avg_admit)
+                stat_collector.add_stat(solver.get_name()+rgx, DOWNLOAD_LAYER, run_name, tr.avg_dl)
+                stat_collector.add_stat(solver.get_name()+rgx, RUNTIME, run_name, t2 - t1)
+                stat_collector.add_stat(solver.get_name()+rgx, CHAIN_BW, run_name, tr.chain_bw)
+                stat_collector.add_stat(solver.get_name()+rgx, REVENUE, run_name, tr.revenue)
+                for rg in tr.res_groups:
+                    rgx = "{:.1f}".format(solver.bw_scaler)
+                    stat_collector2.add_stat(rg, GROUP, rgx, tr.res_groups[rg])
 
     machine_id = "ut"
     fig_test_id = "{}_scaling".format(machine_id)
     inter_arrival = str(inter_arrival).replace(".", "_")
     fig_2 = './result/{}_accept_ia{}'.format(fig_test_id, inter_arrival)
-    stat_collector.write_to_file(fig_2 + '.txt', scaling_factors, 0, ACCEPT_RATIO, algs, 'Share Percentage', ACCEPT_RATIO)
+    stat_collector.write_to_file(fig_2 + '.txt', req_nums, 0, ACCEPT_RATIO, algs, 'Share Percentage', ACCEPT_RATIO)
 
     fig_2 = './result/{}_dl_ia{}'.format(fig_test_id, inter_arrival)
-    stat_collector.write_to_file(fig_2 + '.txt', scaling_factors, 0, DOWNLOAD_LAYER, algs, 'Share Percentage', DOWNLOAD_LAYER)
+    stat_collector.write_to_file(fig_2 + '.txt', req_nums, 0, DOWNLOAD_LAYER, algs, 'Share Percentage', DOWNLOAD_LAYER)
 
     fig_3 = './result/{}_time_ia{}'.format(fig_test_id, inter_arrival)
-    stat_collector.write_to_file(fig_3 + '.txt', scaling_factors, 0, RUNTIME, algs, 'Share Percentage', RUNTIME)
+    stat_collector.write_to_file(fig_3 + '.txt', req_nums, 0, RUNTIME, algs, 'Share Percentage', RUNTIME)
 
     fig_4 = './result/{}_chain_ia{}'.format(fig_test_id, inter_arrival)
-    stat_collector.write_to_file(fig_4 + '.txt', scaling_factors, 0, CHAIN_BW, algs, 'Chaining BW', CHAIN_BW)
+    stat_collector.write_to_file(fig_4 + '.txt', req_nums, 0, CHAIN_BW, algs, 'Chaining BW', CHAIN_BW)
 
     fig_5 = './result/{}_rev_ia{}'.format(fig_test_id, inter_arrival)
-    stat_collector.write_to_file(fig_5 + '.txt', scaling_factors, 0, REVENUE, algs, 'Revenue', REVENUE)
+    stat_collector.write_to_file(fig_5 + '.txt', req_nums, 0, REVENUE, algs, 'Revenue', REVENUE)
 
     fig_6 = './result/{}_ss_ia{}'.format(fig_test_id, inter_arrival)
     stat_collector2.write_to_file(fig_6 + '.txt', scaling_factors, 0, GROUP, algs2, 'GROUP', GROUP)
@@ -341,15 +345,17 @@ def backtrack_test(inter_arrival):
     CHAIN_BW = "Chain (mbps)"
     REVENUE = "Revenue"
     backtrack_vals = [0, 1, 2, 3]
-    solvers = [ FfSolver() ]
+    solvers = []
+    algs = []
     for bv in backtrack_vals:
         solvers.append(GurobiSingleRelax(bv, 1.0, "popularity_learn"))
+        rgx = "{:d}".format(solvers[-1].Gamma)
+        algs.append(solvers[-1].get_name()+rgx)
     stats = {ACCEPT_RATIO: Stat.MEAN_MODE,
              DOWNLOAD_LAYER: Stat.MEAN_MODE,
              CHAIN_BW: Stat.MEAN_MODE,
              RUNTIME: Stat.MEAN_MODE,
              REVENUE: Stat.MEAN_MODE}
-    algs = [s.get_name() for s in solvers]
     stat_collector = StatCollector(algs, stats)
     GROUP = "GROUP"
     stats2 = {GROUP: Stat.SUM_MODE}
@@ -386,15 +392,16 @@ def backtrack_test(inter_arrival):
                     tr = test(solver, reqs)
                     print("Solver: {} got {}".format(solver.get_name(), tr))
                 t2 = process_time()
-                stat_collector.add_stat(solver.get_name(), ACCEPT_RATIO, run_name, tr.avg_admit)
-                stat_collector.add_stat(solver.get_name(), DOWNLOAD_LAYER, run_name, tr.avg_dl)
-                stat_collector.add_stat(solver.get_name(), RUNTIME, run_name, t2 - t1)
-                stat_collector.add_stat(solver.get_name(), CHAIN_BW, run_name, tr.chain_bw)
-                stat_collector.add_stat(solver.get_name(), REVENUE, run_name, tr.revenue)
-                if solver.get_name()[0:2] == "Gr":
-                    for rg in tr.res_groups:
-                        rgx = "{:d}".format(solver.Gamma)
-                        stat_collector2.add_stat(rg, GROUP, rgx, tr.res_groups[rg])
+                rgx = "{:d}".format(solver.Gamma)
+                stat_collector.add_stat(solver.get_name()+rgx, ACCEPT_RATIO, run_name, tr.avg_admit)
+                stat_collector.add_stat(solver.get_name()+rgx, DOWNLOAD_LAYER, run_name, tr.avg_dl)
+                stat_collector.add_stat(solver.get_name()+rgx, RUNTIME, run_name, t2 - t1)
+                stat_collector.add_stat(solver.get_name()+rgx, CHAIN_BW, run_name, tr.chain_bw)
+                stat_collector.add_stat(solver.get_name()+rgx, REVENUE, run_name, tr.revenue)
+                #if solver.get_name()[0:2] == "Gr":
+                for rg in tr.res_groups:
+                    rgx = "{:d}".format(solver.Gamma)
+                    stat_collector2.add_stat(rg, GROUP, rgx, tr.res_groups[rg])
 
     machine_id = "ut"
     fig_test_id = "{}_backtrack".format(machine_id)
@@ -724,7 +731,7 @@ def test_qlearning(inter_arrival):
         STEP_DL_LAYER: Stat.MEAN_MODE,
         RUN_AVG_ADMIT: Stat.MEAN_MODE
     }
-    algs = [s.get_name() for s in solvers]
+    algs = [s.get_name()+s.eviction_strategy for s in solvers]
     stat_collector = StatCollector(algs, stats)
     stat_collector2 = StatCollector(algs, stats2)
     arrival_rate = 1.0 / inter_arrival
@@ -745,17 +752,19 @@ def test_qlearning(inter_arrival):
             if solver.convert_layer:
                 R_ids, R_vols = solver.do_convert_no_share(reqs)
             else:
-                R_ids, R_vols = solver.get_Rid_vol(reqs)
+                R_ids = [i for i in sfc_gen.layers]
+                R_vols = sfc_gen.layers
             solver.set_env(my_net, R_ids, R_vols)
             tr = test(solver, reqs)
-            print("{}-Solver: {} got {} out of {}, dl_vol {}".format(itr, solver.get_name(), tr.avg_admit, req_num, tr.avg_dl))
+            rgx = solver.eviction_strategy
+            print("{}-Solver: {} got {} out of {}, dl_vol {}".format(itr, solver.get_name()+rgx, tr.avg_admit, req_num, tr.avg_dl))
             x_axis2 = list(range(len(tr.run_avg_dl)))
             for i in range(len(tr.run_avg_dl)):
-                stat_collector2.add_stat(solver.get_name(), STEP_DL_LAYER, str(i), tr.run_avg_dl[i])
-                stat_collector2.add_stat(solver.get_name(), RUN_AVG_ADMIT, str(i), tr.run_avg_admit[i])
-            stat_collector.add_stat(solver.get_name(), ACCEPT_RATIO, run_name, tr.avg_admit)
-            stat_collector.add_stat(solver.get_name(), DOWNLOAD_LAYER, run_name, tr.avg_dl)
-            stat_collector.add_stat(solver.get_name(), CHAIN_BW, run_name, tr.chain_bw)
+                stat_collector2.add_stat(solver.get_name()+rgx, STEP_DL_LAYER, str(i), tr.run_avg_dl[i])
+                stat_collector2.add_stat(solver.get_name()+rgx, RUN_AVG_ADMIT, str(i), tr.run_avg_admit[i])
+            stat_collector.add_stat(solver.get_name()+rgx, ACCEPT_RATIO, run_name, tr.avg_admit)
+            stat_collector.add_stat(solver.get_name()+rgx, DOWNLOAD_LAYER, run_name, tr.avg_dl)
+            stat_collector.add_stat(solver.get_name()+rgx, CHAIN_BW, run_name, tr.chain_bw)
 
     machine_id = "ut"
     fig_test_id = "{}_eviction".format(machine_id)
